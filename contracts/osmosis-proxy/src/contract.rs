c6b01e9274f53d1b3c35f5dafb1fed12c751c2bf
Trix <triccs333@gmail>
2024-11-20 18:30:08 -0500

    OP: Migrate to save routes from test mainnet OP into state

--- contracts/osmosis-proxy/src/contract.rs
+++ contracts/osmosis-proxy/src/contract.rs
@@ -1039,8 +1039,14 @@ fn handle_create_denom_reply(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, TokenFactoryError> {
+    //Query routes from the test OP
+    let routes: Vec<SwapRoute> = deps.querier.query_wasm_smart::<Vec<SwapRoute>>(
+        "osmo1968gjpryrmvkydzw47dfdae0p9jzy43p4ckr9geswekm73j4ufkq5tz07q".to_string(),
+        &QueryMsg::GetSwapRoutes {  }
+    )?;
     //Update current routes
-    let routes = SWAP_ROUTES.load(deps.storage)?;
+    SWAP_ROUTES.save(deps.storage, &routes)?;
+    
 
     Ok(Response::default().add_attribute("routes", format!("{:?}", routes)))
 }
